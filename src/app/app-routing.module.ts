3e4815edf4b9f43a198c7ae456a08672dd5ee3cf
merveak648@gmail.com <merveak648@gmail.com>
2021-03-17 19:44:09 +0300

    17.03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
-  {path:"products", component:ProductComponent}
+  {path:"products", component:ProductComponent},
+  {path:"products/category/:categoryId", component:ProductComponent}
+
 ];
 
 @NgModule({
